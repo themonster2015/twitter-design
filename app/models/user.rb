ee39903a68646b73e9cc9f7d5d0b36e05f5b3e7d
qvothi <quynhyen.vothi@gmail.com>
2021-07-24 18:54:40 +0200

    add ability to upload and display photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
   validates :Username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 3, maximum: 25 }
   validates :Fullname, presence: true, length: { minimum: 3, maximum: 100 }
 
+  mount_uploader :Photo, ImageUploader
+  mount_uploader :CoverImage, ImageUploader
+
+
   def self.user_followers(id, curr_user_id)
     Following.where(Followedid: id).where.not(Followerid: curr_user_id).order(created_at: :desc).limit(5)
   end
