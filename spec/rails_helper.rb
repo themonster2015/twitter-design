0ee17f14aed7ce66e7f10e61cbf4017c2baa5aa4
qvothi <quynhyen.vothi@gmail.com>
2021-07-25 14:45:20 +0200

    wrote tests for models

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,7 +63,6 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-  config.include FactoryBot::Syntax::Methods
 end
 
 Shoulda::Matchers.configure do |config|
